17b4d1ad96691af484fd9c7a973f2828a7131860
Irdi Avxhi <irdiavxhi@gmail.com>
2021-11-13 22:11:56 +0100

    iavxhi - adding debug print

--- formatted_prints/src/main.rs
+++ formatted_prints/src/main.rs
@@ -14,4 +14,22 @@ fn main() {
         object="the lazy dog",
         verb="jumps over",
         subject="the quick brown fox");
+
+    // special formatting can be specified after a : 
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+
+    // you can right-align text with specified width. This will output 1. 5 white spaces and a 1
+    println!("{number:>width$}", number=1, width=6);
+    println!("{number:0<width$}", number = 1, width=6);
+    println!("{number:0>width$}", number = 1, width=6);
+
+    // rust even checks to make the correct number of arguments are used
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // create a structure named 'Structure'which contains an i32
+    #[derive(Debug)]
+    struct Structure(i32);
+
+    println!("This struct {:?} wont print... ", Structure(3));
 }
